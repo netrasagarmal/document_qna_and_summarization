9e83f00d5cd3a40bf3575c4ae9f0e28c204f9d59
Sagar Patil <netrasagar12mati@gmail.com>
2025-04-08 21:22:10 +0530

    backend qna development

--- doc_summ_qna_app/backend/doc_summ_qna.py
+++ doc_summ_qna_app/backend/doc_summ_qna.py
@@ -6,11 +6,22 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from typing import List, Tuple, Dict, Union, Optional
 from PyPDF2 import PdfReader
-from langchain_methods import load_docs_file, load_pdf_file, load_txt_file, load_txt, document_chunking
+from langchain_methods import load_docs_file, load_pdf_file, load_txt_file, load_txt, document_chunking, initialize_embeddings, initialize_vector_store
+from langchain_methods import embed_query, retrive_similar_chunks, add_document_to_vector_store
 from langchain_core.documents import Document
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_openai import ChatOpenAI
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+# from openai import OpenAI
+
+INSTRUCTION = """ You are an AI assistant designed to answer user queries based only on the provided context. 
+        If the context contains relevant information."""
+
 
 
 class DocSummAndQnA:
+
     def __init__(self):
         self.file_path: str = None
         self.text_input: str = None
@@ -19,8 +30,10 @@ class DocSummAndQnA:
         self.vectorise_document: List = None
         self.summary_chunk: List[Document] = None
         self.qna_chunks: List[Document] = None
+        self.embedding_model: HuggingFaceBgeEmbeddings = None
+        self.vector_store: FAISS = None
 
-    def load_document(self, file_path=None, document_type=None, text=None)->Optional[List[Document]]:
+    def load_document(self, file_path = None, document_type = None, text = None)->Optional[List[Document]]:
 
         docs = None
         if document_type == "PDF":
@@ -41,10 +54,64 @@ class DocSummAndQnA:
         return docs
     
     # def document_summarize(self,document: List[Document])->str:
+
+    def create_prompt(self, instruction: str = "", query:str = "", context:str = "")->str:
+
+        prompt = """
+        {}
+        
+        {}
+
+        {}
+
+        """.format(instruction, query, context)
+
+        return prompt
         
 
-    # def document_qna(self,document: List[Document])->str:
-    #     pass
+        
+
+    def document_qna(self, query:str)->str:
+
+        similar_chunks = retrive_similar_chunks(self.vector_store, query=query)
+
+        context = ""
+        for chunk in similar_chunks:
+            context = context + "\n" + chunk.page_content
+        
+        prompt = self.create_prompt(instruction=INSTRUCTION, query=query, context=context)
+
+        # formatted_prompt = [
+        #     {"role": "developer", "content": "You are a helpful assistant."},
+        #     {"role": "user", "content": str(prompt)},
+        # ]
+
+        system_prompt = "You are a helpful assistant. Please response to the user queries"
+
+        prompt=[
+            ("system",system_prompt),
+            ("user",prompt)
+        ]
+
+        model_name = "qwen-2.5"
+        api_base = "http://10.159.108.25:3010"
+        api_key = "sk-aDYdRPi_eZGnYm7olBMLaw"
+
+        # openAI LLM
+        llm = ChatOpenAI(
+            model=model_name,
+            openai_api_key=api_key,  # if you prefer to pass api key in directly
+            openai_api_base=api_base
+        )
+
+        response = llm.invoke(prompt)
+
+        # response = llm.invoke(prompt)
+
+        return response
+
+
+        
 
     def document_initial_setup(self, file_path=None, document_type=None, text=None)-> None:
 
@@ -53,13 +120,29 @@ class DocSummAndQnA:
         # Load document
         docs = self.load_document(file_path=file_path, document_type=document_type)
 
+        print("\n\n\n - - - document loaded - - - - - -- - \n\n")
+
         #create chunks for summarization
-        summary_chunk = document_chunking(document=docs, chunk_size=2000, chunk_overlap=150)
+        self.summary_chunk = document_chunking(document=docs, chunk_size=2000, chunk_overlap=150)
+
+        print("\n\n\n - - - summary chunk - - - - - -- - \n\n")
 
         #create chunks for QnA
-        summary_chunk = document_chunking(document=docs, chunk_size=800, chunk_overlap=100)
+        self.qna_chunks = document_chunking(document=docs, chunk_size=800, chunk_overlap=100)
+
+        print("\n\n\n - - - qna chunk done - - - - - -- - \n\n")
 
         #Embed chunks and store in memory
+
+        self.embedding_model = initialize_embeddings()
+
+        print("\n\n\n - - - embedding model initialized - - - - - -- - \n\n")
+
+        self.vector_store = initialize_vector_store(embedding_model=self.embedding_model)
+
+        print("\n\n\n - - - vector store model initialized - - - - - -- - \n\n")
+
+        add_document_to_vector_store(documents=self.qna_chunks, vector_store=self.vector_store)
 
 
 
@@ -69,6 +152,12 @@ if __name__ == "__main__":
     document_type = "PDF"
 
     doc_sum_qna = DocSummAndQnA()
+    # print(1)
+    print("\n\n\n - - - 1 - - - - - -- - \n\n")
 
-    doc = doc_sum_qna.load_document(file_path=file_path, document_type=document_type)
-    print(doc)
+    doc_sum_qna.document_initial_setup(file_path=file_path, document_type=document_type)
+    # print(doc)
+    print("\n\n\n - - - 2 - - - - - -- - \n\n")
+    ans = doc_sum_qna.document_qna(query="what is khan academy making?")
+    print(ans)
+
